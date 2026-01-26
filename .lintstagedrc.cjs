3aa5448678de9d64bc2841641c8ff6c850cab657
Farooq Olanrewaju <ofarooqdev@gmail.com>
2026-01-27 01:29:14 +0300

    chore: improve pre-commit

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,10 @@
 /** @type {import('lint-staged').Config} */
 module.exports = {
-  "*.{js,jsx,ts,tsx,json,md,yml,yaml}": [
+  "*.{js,jsx,ts,tsx}": [
+    "eslint --fix",
+    "prettier -w"
+  ],
+  "*.{json,md,yml,yaml}": [
     "prettier -w"
   ]
 };
