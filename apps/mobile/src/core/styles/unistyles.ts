ae25687ac1eb9c873623aaef23ab93f666d42790
Farooq Olanrewaju <olanrewajufarooq@yahoo.com>
2026-03-12 08:36:47 +0300

    feat(mobile): add react-native-unistyles and react-native-nitro-module

---
+++ apps/mobile/src/core/styles/unistyles.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import { StyleSheet } from "react-native-unistyles";
+
+import { breakpoints } from "./breakpoints";
+import { lightTheme, darkTheme } from "./theme";
+
+type AppBreakpoints = typeof breakpoints;
+
+type AppThemes = {
+  light: typeof lightTheme;
+  dark: typeof darkTheme;
+};
+
+declare module "react-native-unistyles" {
+  export interface UnistylesBreakpoints extends AppBreakpoints {}
+  export interface UnistylesThemes extends AppThemes {}
+}
+
+StyleSheet.configure({
+  breakpoints,
+  themes: {
+    light: lightTheme,
+    dark: darkTheme,
+  },
+  settings: {
+    adaptiveThemes: true,
+  },
+});
